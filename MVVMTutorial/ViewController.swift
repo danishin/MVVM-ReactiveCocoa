d1432c4f6982ab241c7e0e1346c8111ead16bd4c
Daniel Shin <swshin518@hotmail.com>
2015-11-05 12:37:08 +0800

    Initial Commit

---
+++ MVVMTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMTutorial
+//
+//  Created by Daniel Shin on 2015-11-05.
+//  Copyright © 2015 Daniel Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
